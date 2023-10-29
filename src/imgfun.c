2bd2970819b6f85285e38d5d9eeb477f87c50c6c
manjaroman <manjaroman@protonmail.com>
2023-10-29 22:30:58 +0100

    l

--- src/imgfun.c
+++ src/imgfun.c
@@ -33,6 +33,7 @@ int open_pngs(const char *files[], size_t n)
     }
     FILE *pngs[n];
     spng_ctx *ctxs[n];
+    unsigned char *images[n];
     size_t i;
     int ret = 0;
     for (i = 0; i < n; i++)
@@ -44,7 +45,7 @@ int open_pngs(const char *files[], size_t n)
         {
             printf("error code %d opening file %s\n", errno, files[i]);
             ret = 1;
-            goto closeFiles;
+            goto deallocAllPrevious;
         }
         spng_ctx *ctx = spng_ctx_new(0);
         ctxs[i] = ctx;
@@ -52,39 +53,40 @@ int open_pngs(const char *files[], size_t n)
         {
             printf("ERROR: spng_ctx_new() failed.\n");
             ret = 1;
-            goto error;
+            goto deallocFile;
         }
 
         if (spng_set_crc_action(ctx, SPNG_CRC_USE, SPNG_CRC_USE) > 0)
         {
             printf("ERROR: spng_set_crc_action() failed.\n");
             ret = 1;
-            goto error;
+            goto deallocCtx;
         }
+
         size_t lim = 1024 * 1024 * 64;
 
         if (spng_set_chunk_limits(ctx, lim, lim) > 0)
         {
             printf("ERROR: spng_set_chunk_limits() failed.\n");
             ret = 1;
-            goto error;
+            goto deallocCtx;
         }
 
         if (spng_set_png_file(ctx, png) > 0)
         {
             printf("ERROR: spng_set_png_file() failed.\n");
             ret = 1;
-            goto error;
+            goto deallocCtx;
         }
 
         struct spng_ihdr ihdr;
         ret = spng_get_ihdr(ctx, &ihdr);
 
-        if (ret)
+        if (ret > 0)
         {
             printf("spng_get_ihdr() failed: error %s\n", spng_strerror(ret));
             ret = 1;
-            goto error;
+            goto deallocCtx;
         }
 
         const char *color_name = color_type_str(ihdr.color_type);
@@ -100,26 +102,55 @@ int open_pngs(const char *files[], size_t n)
                "interlace method: %u\n",
                ihdr.compression_method, ihdr.filter_method, ihdr.interlace_method);
 
-        struct spng_plte plte = {0};
+        int fmt = SPNG_FMT_PNG;
+        if (ihdr.color_type == SPNG_COLOR_TYPE_INDEXED)
+            fmt = SPNG_FMT_RGB8;
 
-        ret = spng_get_plte(ctx, &plte); 
+        size_t image_size;
+        ret = spng_decoded_image_size(ctx, fmt, &image_size);
 
-        if (ret || ret != SPNG_ECHUNKAVAIL) 
+        if (ret > 0)
         {
-            printf("spng_get_plte() error: %s\n", spng_strerror(ret));
-            goto error;
+            printf("spng_decode_image_size() error: %s\n", spng_strerror(ret));
+            ret = 1;
+            goto deallocCtx;
         }
-        
+
+        unsigned char *image;
+        image = malloc(image_size);
+
+        if (image == NULL)
+        {
+            printf("malloc error: %d. buy more RAM lol!\n", errno);
+            ret = 1;
+            goto deallocCtx;
+        }
+
+        images[i] = image;
+
+        ret = spng_decode_image(ctx, image, image_size, SPNG_FMT_RGBA8, 0);
+        if (ret > 0)
+        {
+            printf("spng_decode_image() error: %s\n", spng_strerror(ret));
+            ret = 1;
+            goto deallocImage;
+        }
+
+        continue;
+    deallocImage:
+        free(image);
+    deallocCtx:
+        spng_ctx_free(ctx);
+    deallocFile:
+        fclose(png);
+        goto deallocAllPrevious;
     }
 
-error:
-    for (size_t j; j < i; j++)
+deallocAllPrevious:
+    for (size_t j = 0; j < i; j++)
     {
+        free(images[j]);
         spng_ctx_free(ctxs[j]);
-    }
-closeFiles:
-    for (size_t j; j < i; j++)
-    {
         if (fclose(pngs[j]) < 0)
         {
             printf("error code %d closing file %s\n", errno, files[j]);
